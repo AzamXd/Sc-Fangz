0125330002bb5b51ed59a32cfccdc59c09072185
luxxyanjaymabar <luxxyanjaymabar.luxxyxyzvfx@gmail.com>
2022-09-04 18:52:12 +0700

    luxxyxyz

---
+++ plugins/exp-daftar.js
@@ -0,0 +1,82 @@
+import { createHash } from 'crypto' 
+ let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i 
+ let handler = async function (m, { text, usedPrefix, command }) { 
+         function pickRandom(list) { 
+   return list[Math.floor(Math.random() * list.length)] 
+ } 
+         let namae = conn.getName(m.sender) 
+         const sections = [ 
+         { 
+         title: "Select Your Age Here !", 
+         rows: [ 
+             {title: "Random Years", rowId: '.daftar ' + namae + '.' + pickRandom(['30','29','28','27','26','25','24','23','22','21','20','19','18','17','16','15','14','13','12','11','10','9'])} 
+         ] 
+     }, 
+     { 
+         title: "O L D", 
+         rows: [ 
+             {title: "30 Years", rowId: '.daftar ' + namae + '.30 '}, 
+             {title: "29 Years", rowId: '.daftar ' + namae + '.29 '}, 
+             {title: "28 Years", rowId: '.daftar ' + namae + '.28 '}, 
+         {title: "27 Years", rowId: '.daftar ' + namae + '.27 '}, 
+         {title: "26 Years", rowId: '.daftar ' + namae + '.26 '}, 
+         {title: "25 Years", rowId: '.daftar ' + namae + '.25 '}, 
+         {title: "24 Years", rowId: '.daftar ' + namae + '.24 '}, 
+         {title: "23 Years", rowId: '.daftar ' + namae + '.23 '}, 
+         {title: "22 Years", rowId: '.daftar ' + namae + '.22 '}, 
+         {title: "21 Years", rowId: '.daftar ' + namae + '.21 '} 
+         {title: "20 Years", rowId: '.daftar ' + namae + '.20 '}, 
+         {title: "19 Years", rowId: '.daftar ' + namae + '.19 '}, 
+         {title: "18 Years", rowId: '.daftar ' + namae + '.18 '}, 
+         {title: "17 Years", rowId: '.daftar ' + namae + '.17 '}, 
+         {title: "16 Years", rowId: '.daftar ' + namae + '.16 '}, 
+         {title: "15 Years", rowId: '.daftar ' + namae + '.15 '}, 
+         {title: "14 Years", rowId: '.daftar ' + namae + '.14 '}, 
+         {title: "13 Years", rowId: '.daftar ' + namae + '.13 '}, 
+         {title: "12 Years", rowId: '.daftar ' + namae + '.12 '}, 
+         {title: "11 Years", rowId: '.daftar ' + namae + '.11 '}, 
+         {title: "10 Years", rowId: '.daftar ' + namae + '.10 '}, 
+         {title: "9 Years", rowId: '.daftar ' + namae + '.9 '} 
+         ] 
+     }, 
+ ] 
+  
+ const listMessage = { 
+   text: `Please select your age at the bottom button...\n*Your Name:* ${conn.getName(m.sender)}\nWant a costume name? type *${usedPrefix + command} yourname.age*`, 
+   footer: global.wm, 
+   title: "━━━━「 Registration 」━━━━", 
+   buttonText: "Click Here !", 
+   sections 
+ } 
+  
+   let user = global.db.data.users[m.sender] 
+   if (user.registered === true) throw `[💬] Kamu sudah terdaftar\nMau daftar ulang? *${usedPrefix}unreg <SERIAL NUMBER>*` 
+   if (!Reg.test(text)) return conn.sendMessage(m.chat, listMessage, m) 
+   let [_, name, splitter, age] = text.match(Reg) 
+   if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)' 
+   if (!age) throw 'Umur tidak boleh kosong (Angka)' 
+   age = parseInt(age) 
+   if (age > 30) throw 'WOI TUA (。-`ω´-)' 
+   if (age < 5) throw 'Halah dasar bocil' 
+   user.name = name.trim() 
+   user.age = age 
+   user.regTime = + new Date 
+   user.registered = true 
+   let sn = createHash('md5').update(m.sender).digest('hex') 
+   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.fromMe ? conn.user.jid : m.sender 
+   m.reply(` 
+ ━━━━ 「 *Successful Registration* 」━━━━ 
+  
+ ╭─• 〘 INFO 〙 
+ │✧ *Name:* ${name} 
+ │✧ *Age:* ${age} Years 
+ │✧ *Sn:* ${sn} 
+ ▣──────··· 
+ `.trim()) 
+ } 
+ handler.help = ['daftar', 'register'].map(v => v + ' <nama>.<umur>') 
+ handler.tags = ['xp'] 
+  
+ handler.command = /^(daftar|verify|reg(ister)?)$/i 
+  
+ export default handler
---
+++ plugins/info-script.js
@@ -0,0 +1,27 @@
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => { 
+ let text = `*${htki} CIE CARI SCRIPT ${htka}* 
+ 
+ Di Youtube Bnyak, Ini cuma nge recode campuran
+kalau mau make sc ini cek link di bawah ini yo
+
+Youtube: FangzXD
+ ` 
+ const templateButtons = [ 
+     {index: 1, urlButton: {displayText: '🐈 Github', url: sgh}}, 
+     {index: 2, urlButton: {displayText: '🌎 Website', url: swb}}, 
+     {index: 3, quickReplyButton: {displayText: 'OWNER', id: '.owner'}}, 
+     {index: 4, quickReplyButton: {displayText: 'SEWA BOT', id: '.sewa'}}, 
+ ] 
+ let tm = { 
+ text: text, 
+ footer: global.wm, 
+ templateButtons: templateButtons, 
+ image: thumb 
+ } 
+ conn.sendMessage(m.chat, tm, m) 
+ } 
+ handler.help = ['sc'] 
+ handler.tags = ['info'] 
+ handler.command = /^(s(ourcode|c))$/i 
+  
+ export default handler
