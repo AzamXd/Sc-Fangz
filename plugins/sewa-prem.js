4f4e77ea8704a96270a53d530471b4bb7bf803c0
luxxyanjaymabar <luxxyxyzvfx@gmail.com>
2022-09-07 20:36:56 +0700

    Update sewa-prem.js

--- plugins/sewa-prem.js
+++ plugins/sewa-prem.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `${htki} PREMIUM ${htka}
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: 'Klick untuk berlangganan', url: 'https://wa.me/p/5240667609353116/62895394988123'}},
+    {index: 1, urlButton: {displayText: 'Klick untuk berlangganan', url: 'https://wa.me/p/5772184646149444/6288210072756'}},
     {index: 4, quickReplyButton: {displayText: '🗳️SEWA BOT', id: '.belibot'}},
 ]
 let tm = {
