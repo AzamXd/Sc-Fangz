3af1c183f212fdd10f586eb4e09dc12a88cbbb98
luxxyanjaymabar <luxxyxyzvfx@gmail.com>
2022-09-23 08:09:51 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,28 +1,36 @@
-import { promises } from 'fs'
-import { join } from 'path'
-import { xpRange } from '../lib/levelling.js'
-import moment from 'moment-timezone'
-import os from 'os'
-import fs from 'fs'
-import fetch from 'node-fetch'
-let handler = async (m, { conn, args, command }) => {
-let totalf = Object.values(global.plugins).filter(
-    (v) => v.help && v.tags
-  ).length;
- await conn.sendButton(m.chat, `꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝  Nyari Ownerku Yakak?? >//<, Nih Nomornya wa.me/6288210072756\n`,after + '\n\n' + titlebot, ow, [['Luxxy','.menu']], m, {
-contextInfo: { externalAdReply :{
-                        mediaUrl: '',
-                        mediaType: 2,
-                        description: 'anu',
-                        title: botclick,
-                        body: bottime,          previewType: 0,
-                        sourceUrl: swo
-                      }}
-})
+import fs from 'fs';
+import crypto from 'crypto'
+let handler = async (m, { conn, text, usedPrefix }) => {
+
+    let sewa = `
+  〔 llı OWNER ${namebot} ıll 〕
+    
+Nama: ${nameown}
+Umur: 14
+Asal: Jakarta Timur
+Nomor: wa.me/${nomorown}
+
+${botdate}
+`
+
+    let img1 = fs.readFileSync('./img/atas.png');
+    let img2 = fs.readFileSync('./img/bawah.png');
+    return conn.sendButton(m.chat, hiasan, sewa, img1, [["menu", usedPrefix + "menu"], ["profile", usedPrefix + "profile"]], m, {
+        contextInfo: {
+            externalAdReply: {
+                mediaUrl: "https://instagram.com/luxxyxyz",
+                mediaType: "VIDEO",
+                title: hiasan,
+                body: bottime,
+                previewType: 0,
+                thumbnail: img1
+            }
+        }
+    })
 }
+handler.help = ['owner']
+handler.tags = ['Bot']
 
+handler.command = /^(owner|creator)$/i
 
-handler.help = ['owner','creator']
-handler.tags = ['info']
-handler.command = /^owner|creator$/i
 export default handler
